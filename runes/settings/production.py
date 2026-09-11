4b3a5ec38fac453eba2392b0607edfa3403585f9
j4lib <julia.beck@lir.gu.se>
2026-09-11 15:56:42 +0200

    Stop using Manifest version for now as it produces errors in production.

--- runes/settings/production.py
+++ runes/settings/production.py
@@ -12,7 +12,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
